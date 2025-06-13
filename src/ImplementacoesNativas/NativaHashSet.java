4214229b2fafd6f079f41f066f98059768358317
willUlisses <williamcavalcante06@gmail.com>
2025-06-13 15:29:33 -0300

    HashSet Nativa

--- src/ImplementacoesNativas/NativaHashSet.java
+++ src/ImplementacoesNativas/NativaHashSet.java
@@ -1,4 +1,32 @@
 package ImplementacoesNativas;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class NativaHashSet {
+    public static void main(String[] args) {
+
+        Set<Integer> myHashSet = new HashSet<>();
+
+        myHashSet.add(10);
+        myHashSet.add(20);
+        myHashSet.add(30);
+        myHashSet.add(40);
+        myHashSet.add(50);
+
+        System.out.println(myHashSet);
+
+
+        Set<Integer> myTreeSet = new TreeSet<>();
+
+        myTreeSet.add(10);
+        myTreeSet.add(20);
+        myTreeSet.add(30);
+        myTreeSet.add(40);
+        myTreeSet.add(50);
+
+        System.out.println(myTreeSet);
+
+    }
 }
