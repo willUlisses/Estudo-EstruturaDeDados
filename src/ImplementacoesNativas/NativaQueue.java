d9f688ca96af6d18293139f62d153716d8265af2
willUlisses <williamcavalcante06@gmail.com>
2025-06-13 13:27:37 -0300

    Queue Nativa

--- src/ImplementacoesNativas/NativaQueue.java
+++ src/ImplementacoesNativas/NativaQueue.java
@@ -1,4 +1,27 @@
 package ImplementacoesNativas;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class NativaQueue {
+    public static void main(String[] args) {
+
+        Queue<Integer> myQueue = new LinkedList<>();
+
+        myQueue.add(10);
+        myQueue.add(20);
+        myQueue.add(30);
+        myQueue.add(40);
+        myQueue.add(50);
+
+        System.out.println(myQueue);
+        myQueue.remove();
+        System.out.println(myQueue);
+        System.out.println(myQueue.offer(60));
+        System.out.println(myQueue);
+        System.out.println(myQueue.peek());
+        System.out.println(myQueue.poll());
+        System.out.println(myQueue);
+
+    }
 }
